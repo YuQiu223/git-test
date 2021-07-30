7213a57be852399cd5751f5957f49d8dcacb8ce3
xiaohe <xiaohe@atguigu.com>
2021-07-30 10:47:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author shkstart
+ * @create 2021-07-30 10:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
